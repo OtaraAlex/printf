5607fa2b91419c5ecfbf702565719995de80bc09
Sinned254 <dennis1kosgei@gmail.com>
2022-10-18 11:32:13 -0700

    add prototypes to main.h

--- main.h
+++ main.h
@@ -3,6 +3,31 @@
 #include <unistd.h>
 #include <stdarg.h>
 #include <stdio.h>
+
+/**
+ * struct print - structure for printing varoious types
+ * @t: type to print
+ * @f: function to print
+ */
+typedef struct print
+{
+	char *t;
+	int (*f)(va_list);
+} print_t;
+
+int print_c(va_list c);
+int print_s(va_list s);
+int print_i(va_list i);
+int print_d(va_list d);
+int print_u(va_list u);
+int print_b(va_list b);
+int print_o(va_list o);
+int print_x(va_list x);
+int print_X(va_list X);
+int print_p(va_list p);
+int print_S(va_list S);
+int print_r(va_list r);
+int print_R(va_list R);
 int _putchar(char c);
 int _strlen(char *s);
 int _puts(char *s);
